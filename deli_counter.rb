4d241ab21f0e7686f17d7678363cd4e1e17b4fb9
Kevin C Malin <racmalin@yahoo.com>
2020-04-27 01:24:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,11 @@
 def line(array)
   line_length = array.length
  if line_length == 0 
-   puts "the line is currently empty."
- else line_order = "The line is currently:"
+   puts "The line is currently empty."
+ else 
+   line_order = "The line is currently:"
    array.each_with_index do |name, i|
-     line_order = line_order + "#{i+} . " + name
+     line_order = line_order + " #{i+1}. " + name
    end
    puts line_order
 end
