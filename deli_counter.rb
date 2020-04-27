1970b1b4adb30612184d6251f3730babb5387b04
Kevin C Malin <racmalin@yahoo.com>
2020-04-27 01:21:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,10 @@
-# Write your code here.
+def line(array)
+  line_length = array.length
+ if line_length == 0 
+   puts "the line is currently empty."
+ else line_order = "The line is currently:"
+   array.each_with_index do |name, i|
+     line_order = line_order + "#{i+} . " + name
+   end
+   puts line_order
+end
